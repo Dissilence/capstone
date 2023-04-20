a54c3ea698ed92836bb37383e8db510fe7032424
Preshen Govender <Preshengovender12271@gmail.com>
2023-04-20 15:00:36 +1200

    Created my react application

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Vermillion</a>
+      </header>
+      <main>list of games</main>
+    </div>
+  );
+}
+
+export default App;
