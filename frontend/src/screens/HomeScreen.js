4ff6bd824abeacd8f388c73ba7a7b35a96272e04
Preshen Govender <Preshengovender12271@gmail.com>
2023-05-03 01:29:19 +1200

    made order models and routes as well as user routes updated with the checkoutSteps

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 //import data from '../data';
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
