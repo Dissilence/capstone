b5ee68ce32fed69ca3139a03f42251994b55a90a
Preshen Govender <Preshengovender12271@gmail.com>
2023-05-03 17:35:21 +1200

    created order history and user profile editing page, allowing user to change details such as email, user name and password. also created heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import expressAsyncHandler from 'express-async-handler';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
@@ -24,11 +23,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(_dirname, '/frontend/build/index.html'))
+);
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
